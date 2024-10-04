2118299da0aab120893f8a89307adf0d7a17a63b
Rizki Ramadandi <rramadandi16@gmail.com>
2024-10-04 08:21:47 +0700

    add PUT & DELETE methods, and change Checked to pointer

--- main.go
+++ main.go
@@ -6,28 +6,38 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// todo represents data about a record todo.
+// todo struct.
 type todo struct {
 	ID      string `json:"id"`
 	Name    string `json:"name"`
-	Checked bool   `json:"is_checked"`
+	Checked *bool  `json:"is_checked"`
 }
 
-// todos slice to seed record todo data.
+// default value for assigning boolean addresses.
+var DefaultTrue, DefaultFalse = true, false
+
+// todos data collection.
 var todos = []todo{
-	{ID: "1", Name: "Learn Golang", Checked: true},
-	{ID: "2", Name: "Learn MongoDB", Checked: true},
-	{ID: "3", Name: "Build Simple REST API with Golang & MongoDB", Checked: false},
+	{ID: "1", Name: "Learn Golang", Checked: &DefaultTrue},
+	{ID: "2", Name: "Learn MongoDB", Checked: &DefaultTrue},
+	{ID: "3", Name: "Build Simple REST API with Golang & MongoDB", Checked: &DefaultFalse},
 }
 
 // postTodos adds a todo from JSON received in the request body.
 func postTodos(c *gin.Context) {
 	var newTodo todo
 
-	// Call BindJSON to bind the received JSON to
-	// newTodo.
+	// Call BindJSON to bind the received JSON to newTodo.
 	if err := c.BindJSON(&newTodo); err != nil {
 		return
+	}
+
+	// check for ID duplication before adding newTodo.
+	for _, t := range todos {
+		if t.ID == newTodo.ID {
+			c.IndentedJSON(http.StatusConflict, gin.H{"message": "ID already exist"})
+			return
+		}
 	}
 
 	// Add the new TODO to the slice.
@@ -35,26 +45,68 @@ func postTodos(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newTodo)
 }
 
-// getTodoByID locates the todo whose ID value matches the id
-// parameter sent by the client, then returns that todo as a response.
+// getTodoByID locates the todo whose ID value matches the id parameter sent by the client, then returns that todo as a response.
 func getTodoByID(c *gin.Context) {
-	// get parameter id, different from query
 	id := c.Param("id")
 
-	// Loop over the list of todos, looking for
-	// an todo whose ID value matches the parameter.
+	// Loop over the list of todos, looking for a todo whose ID value matches the parameter.
 	for _, a := range todos {
 		if a.ID == id {
 			c.IndentedJSON(http.StatusOK, a)
 			return
 		}
 	}
+
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
 }
 
 // getTodos responds with the list of all todos as JSON.
 func getTodos(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, todos)
+}
+
+// updateTodos updating todo whose ID matches the id parameter esent by the client.
+func updateTodos(c *gin.Context) {
+	id := c.Param("id")
+
+	var newTodo todo
+
+	// Call BindJSON to bind the received JSON to newTodo.
+	if err := c.BindJSON(&newTodo); err != nil {
+		return
+	}
+
+	// Loop over the list of todos, looking for a todo whose ID value matches the parameter.
+	for idx, a := range todos {
+		if a.ID == id {
+			if newTodo.Name != "" {
+				todos[idx].Name = newTodo.Name
+			}
+			if newTodo.Checked != nil {
+				todos[idx].Checked = newTodo.Checked
+			}
+			c.IndentedJSON(http.StatusOK, gin.H{"message": "todo's updated"})
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
+}
+
+// deleteTodos delete todo whose ID matches the id parameter by the client.
+func deleteTodos(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of todos, looking for a todo whose ID value matches the parameter.
+	for idx, a := range todos {
+		if a.ID == id {
+			todos = append(todos[:idx], todos[idx+1:]...)
+			c.IndentedJSON(http.StatusOK, gin.H{"message": "todo's deleted"})
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
 }
 
 func main() {
@@ -66,6 +118,12 @@ func main() {
 
 	// POST routes
 	router.POST("/todos", postTodos)
+
+	// PUT routes
+	router.PUT("/todos/:id", updateTodos)
+
+	// DELETE routes
+	router.DELETE("/todos/:id", deleteTodos)
 
 	router.Run("localhost:8080")
 }
